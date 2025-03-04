fd8472fd7dbc983ae9c91bc5bac81dcb744479bd
shaunpour <scpour@gmail.com>
2025-03-04 10:01:00 -0500

    Added game loop so that a basic black screen opens and stays open as well as automatically terminating the program if the user closes the game window

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 from constants import *
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game = True
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill('Black')
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
