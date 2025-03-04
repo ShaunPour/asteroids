52fbf14baf7b7034dc2d93b396b0894ea1876724
shaunpour <scpour@gmail.com>
2025-03-04 12:30:39 -0500

    spawned player object onscreen

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock=pygame.time.Clock()
     clock
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
 
         screen.fill('Black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
