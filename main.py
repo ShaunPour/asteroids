a629eaca35b37295ed917d5e648b21f0440a5c60
shaunpour <scpour@gmail.com>
2025-03-04 11:12:08 -0500

    Added deltatime tick counter

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pygame
 from constants import *
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game = True
-    while game:
+    clock=pygame.time.Clock()
+    clock
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill('Black')
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
